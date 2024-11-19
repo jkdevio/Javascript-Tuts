d20d6c1c62b4ba34a3e863ed099d4d505563c625
Jonathan-Kalu <jonathankalu313@gmail.com>
2024-11-19 04:53:48 +0100

    Third Commit

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 alert("Good morning");
 alert("Nate");
+
+
+let firstSalary = 100;
+let lunchMeal = 20;
+let dinnerMeal = 50;
+let finalSalary = 200;
+
+console.log(firstSalary - lunchMeal - dinnerMeal +  finalSalary);
