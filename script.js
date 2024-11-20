800ba6678b670bad16505eb8715b055b6147e4aa
Jonathan-Kalu <jonathankalu313@gmail.com>
2024-11-20 05:12:37 +0100

    Updated calculations

--- script.js
+++ script.js
@@ -11,10 +11,22 @@ console.log(firstSalary - lunchMeal - dinnerMeal +  finalSalary);
 
 const calculateInCents = 2095;
 const calculateInCents2 = 799;
+const calculateInCents3 = 289;
 
 let totalConversion = (calculateInCents + calculateInCents2) / 100;
 
 let totalConversion2 = Math.round((calculateInCents + calculateInCents2)* 0.1) / 100;
 
+let totalConversion3 = (calculateInCents + calculateInCents2 + calculateInCents3) / 100;
+
 console.log(totalConversion);
 console.log(totalConversion2);
+console.log(totalConversion3);
+
+let totalCost1 = 10; 
+let totalCost2 = 8;
+let totalCost3 = 5;
+
+let totalCalculations = ((totalCost1) + (totalCost2)* 3 + (totalCost3));
+
+console.log(totalCalculations);
