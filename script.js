2639827722edb508e1defe68d104a58737f85545
Jonathan-Kalu <jonathankalu313@gmail.com>
2024-11-20 04:37:52 +0100

    Updated Conversion Calculations

--- script.js
+++ script.js
@@ -8,3 +8,10 @@ let dinnerMeal = 50;
 let finalSalary = 200;
 
 console.log(firstSalary - lunchMeal - dinnerMeal +  finalSalary);
+
+const calculateInCents = 2095;
+const calculateInCents2 = 799;
+
+let totalConversion = (calculateInCents + calculateInCents2) / 100;
+
+console.log(totalConversion)
